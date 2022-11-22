1df29bea11ddec655283ff8742e64f04e9c7746d
Max-lindberg <112978463+Max-lindberg@users.noreply.github.com>
2022-11-22 20:25:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from time import sleep
+import random
+
+print("Welcome to Xflick community project")
+
+sleep(1)
+
+logs = input("How many Discord logs do you want?")
+
+if logs is int:
+    print("Ok")
+    print("1% generated")
+    sleep(1)
+    print("24% generated")
+    sleep(2)
+    print("45% generated")
+    sleep(0.7)
+    print("79% generated")
+    sleep(0.9)
+    print("100% generated")
+
+
+
+def get_log():
+    
+
+
+
+    print(log)
+
+
