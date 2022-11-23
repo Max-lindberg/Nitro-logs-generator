1214e52ccbee64f534d225b6284ea8468013aa0f
Max lindberg <112978463+Max-lindberg@users.noreply.github.com>
2022-11-23 11:16:21 +0100

    updated
    Just the start

--- main.py
+++ main.py
@@ -4,9 +4,28 @@ import string
 
 print("Welcome to Xflick community project")
 
+print("____  ___  _____ .__   .__          __                                                        .__   __            
+\   \/  /_/ ____\|  |  |__|  ____  |  | __      ____   ____    _____    _____   __ __   ____  |__|_/  |_  ___.__. 
+ \     / \   __\ |  |  |  |_/ ___\ |  |/ /    _/ ___\ /  _ \  /     \  /     \ |  |  \ /    \ |  |\   __\<   |  | 
+ /     \  |  |   |  |__|  |\  \___ |    <     \  \___(  <_> )|  Y Y  \|  Y Y  \|  |  /|   |  \|  | |  |   \___  | 
+/___/\  \ |__|   |____/|__| \___  >|__|_ \     \___  >\____/ |__|_|  /|__|_|  /|____/ |___|  /|__| |__|   / ____| 
+      \_/                       \/      \/         \/              \/       \/             \/             \/      ")
+
 sleep(1)
 
-logs = input("How many Discord logs do you want?")
+logs = input("How many Discord logs do you want? ")
+
+print("Ok")
+print("1% generated")
+sleep(1)
+print("24% generated")
+sleep(2)
+print("45% generated")
+sleep(0.7)
+print("79% generated")
+sleep(0.9)
+print("100% generated")
+
 
 def generat_code():
   return randomword(27)
@@ -38,18 +57,4 @@ while i < int(logs):
   print("https://discord.com/billing/promotions/" + generat_code())
   i += 1
 
-file1.close()  
- 
-# initializing size of string
-#N = 7
- 
-# using random.choices()
-# generating random strings
-#res = ''.join(random.choices(string.ascii_uppercase +
-#                             string.digits, k=N))
- 
-# print result
-#print("The generated random string : " + str(res))
-
-
-
+file1.close()
